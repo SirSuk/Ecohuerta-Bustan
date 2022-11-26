64fbb419672d84a15ade5c6676e8e21bb7af0e83
Radif El noun <siriosukur@gmail.com>
2022-11-26 23:17:00 +0100

    new wallpaper & text component fontfamily

--- src/components/texto/Texto.jsx
+++ src/components/texto/Texto.jsx
@@ -3,7 +3,10 @@ import './Texto.css'
 
 export const Texto = () => {
   return (
-    <div><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur itaque necessitatibus omnis rerum esse provident fugit tempora rem iure. Odio repudiandae consequuntur, ab consequatur numquam delectus hic, in cupiditate enim incidunt, nemo magni et libero? Neque veritatis, autem excepturi id tenetur ea, eius, asperiores commodi libero odit ex in soluta.</p>
+    <div>
+        <h1> ¿Que es EcoBustan? </h1>
+        
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur itaque necessitatibus omnis rerum esse provident fugit tempora rem iure. Odio repudiandae consequuntur, ab consequatur numquam delectus hic, in cupiditate enim incidunt, nemo magni et libero? Neque veritatis, autem excepturi id tenetur ea, eius, asperiores commodi libero odit ex in soluta.</p>
     </div>
   )
 }
