d53f9341567fdfbe2a9acd0cc12fa01198314dfe
Radif El noun <siriosukur@gmail.com>
2022-11-27 00:50:15 +0100

    nuevo hero

--- src/components/texto/Texto.jsx
+++ src/components/texto/Texto.jsx
@@ -4,9 +4,9 @@ import './Texto.css'
 export const Texto = () => {
   return (
     <div>
-        <h1> ¿Que es EcoBustan? </h1>
-        
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur itaque necessitatibus omnis rerum esse provident fugit tempora rem iure. Odio repudiandae consequuntur, ab consequatur numquam delectus hic, in cupiditate enim incidunt, nemo magni et libero? Neque veritatis, autem excepturi id tenetur ea, eius, asperiores commodi libero odit ex in soluta.</p>
+        <h2 className="texto__h2"> ¿Que es EcoBustan? </h2>
+        <p> Es un proyecto en un pequeño terreno de 600 m2, en el que se busca sembrar en ecológico todo tipo de frutas y hortalizas, de forma 100% sostenible, atrayendo fauna benéfica, aprovechándonos de técnicas de Permacultura y Agroecología , la mejora de suelos, y con el objetivo final de crear una red de venta local de cercanía.</p>
+       
     </div>
   )
 }
